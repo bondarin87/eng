922ba0dc767eb472dc86af1ca986b617825bca05
bondarin87 <110008441+bondarin87@users.noreply.github.com>
2024-12-11 13:37:17 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ let verb = {
     2:['get','obtener'],
     3:['make','hacer']
 }
-let arr1 = ['have','do','get','make','know','take','see','come','think','look','want','give','use','find','tell','ask','work','call','try','need','feel','become','leave'];
-let arr2 = ['tener','hacer','obtener','hacer','saber/conocer','tomar','ver','venir','pensar','mirar','querer','dar','usar','encontrar','contar','preguntar','trabajar','llamar','intentar/probar','necesitar','sentir','convertirse','dejar']
+let arr1 = ['have','do','get','make','know','take','see','come','think','look','want','give','use','find','tell','ask','work','call','try','need','feel','become','leave','put','mean','keep','let','begin','seem','help','talk','turn','start','show','hear','play','run','move','like','live','belive','hold','bring','happen','write','sit','stand'];
+let arr2 = ['tener','hacer','obtener','hacer','saber/conocer','tomar','ver','venir','pensar','mirar','querer','dar','usar','encontrar','contar','preguntar','trabajar','llamar','intentar/probar','necesitar','sentir','convertirse','dejar','poner','significar','mantener','permitir/dejar','empezar','parecer','ayudar','hablar','girar','comenzar','mostrar','oir','jugar','correr','mover','gustar','vivir','creer','sostener','traer','suseder','escribir','sentarse','estar de pie']
 
 let elem = document.querySelector('.elem');
 let elem1 = document.querySelector('elem1');
